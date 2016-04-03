fc58440f9e062d7116469288713365d45d8c7c5a
Josh Errickson <josh@errickson.net>
2016-04-03 10:08:37 -0400

    Documented testing slopes per subgroup

--- statistics/test_slope_in_all_subgroups/slope_subgroups.R
+++ statistics/test_slope_in_all_subgroups/slope_subgroups.R
@@ -6,5 +6,11 @@ library(lsmeans)
 # Obtain estimated slope in each subgroup.
 # Requires continuous*categorical interaction to be interesting
 trends <- lstrends(mod, "Month", "Solar.R")
-# Test between each subgroup
+# trends now contains the effect of Solar.R in each Month. This is
+# equivalent to re-running the model 5 times, with each different
+# Month as baseline.
+
+# Now we can test whether the effect of Solar.R differs in each month.
 pairs(trends)
+# All p.values > .05, so there's no evidence that the effect differs,
+# although Month5 vs Month8 is showing some evidence.
